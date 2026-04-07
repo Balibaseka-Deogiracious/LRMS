76d7b6bc47baf7bb397e487dc0e5f57f51fdb035
gratiuslee <gratiuslee3@gmail.com>
2026-04-07 09:21:40 +0300

    connected the database online

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
-        "http://lrms-omega.vercel.app",
+        "https://lrms-omega.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
