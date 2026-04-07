5d975cc72995ae2a29fcc51c6c4afa152e9853e2
gratiuslee <gratiuslee3@gmail.com>
2026-04-07 09:57:05 +0300

    updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,14 +17,7 @@ app = FastAPI(title="Library Management System API", version="1.0.0")
 # Configure CORS to allow frontend requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        # "http://localhost:5173",
-        # "http://localhost:3000",
-        # "http://127.0.0.1:5173",
-        # "http://127.0.0.1:3000",
-        # "https://lrms-omega.vercel.app",
-        "*"
-    ],
+    allow_origins=["https://lrms-two.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
